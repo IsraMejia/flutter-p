6bc18cf63b94ad131c2f2a2a29afe6c64fafcc22
israel <israel7sk8@gmail.com>
2020-02-03 17:04:38 -0600

    flutter

---
+++ Actividades/tercerReto/anuerico/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Analisis Numerico 0.1',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        body: Stack(
+          children : <Widget>[
+
+          ],
+        ),
+      ),
+    );
+}
+
+}
---
+++ Actividades/tercerReto/anuerico/lib/operations.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Operations Extends class name extends StatelessWidget {
+  const name({Key key}) : super(key: key);
+  
+  String titleOperation = "Permutacion";
+  String description = "Es una operacion.....";
+  bool iconOperation = true;
+
+  Operations(this.titleOperation, this.description, this.iconOperation);
+
+  @override
+  Widget build(BuildContext context) {
+    
+    
+    return Container(
+      child: Row(
+
+      )
+    );
+  }
+}
